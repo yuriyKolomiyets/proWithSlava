5fcadf887870c50eed9b4f185c29c9a8b33a87e2
yuriykolomiyets <juriy121@gmail.com>
2018-08-26 12:21:54 +0300

    task compleated

--- src/test/java/week1/db/TestSqlUserService.java
+++ src/test/java/week1/db/TestSqlUserService.java
@@ -14,9 +14,7 @@ import static org.junit.Assert.*;
 
 public class TestSqlUserService {
 
-    static final String JDBC_DRIVER = "org.h2.Driver";
     static final String DB_URL = "jdbc:h2:~/test";
-
     static final String USER = "sa";
     static final String PASS = "";
 
